61d9326a7382c08e0023581463c23981ddf5b44a
nbeelahamadeen <nbeelahamadeen@gmail.com>
2023-01-05 11:26:16 -0600

    added working links for POSTS

--- app.js
+++ app.js
@@ -3,30 +3,71 @@ const app = express();
 const postBank = require('./postBank');
 
 app.use(express.static('public'));
+const posts = postBank.list();
 
 app.get('/', (req, res) => {
   //TODO - get all the posts
-  const posts = postBank.list();
+  //  const posts = postBank.list();
   // console.log(posts);
   res.send(`
-    <html>
-      <head>
-        <link rel='stylesheet' href='/style.css' />
-        <title>Wizard News</title>
-      </head>
-      <body>
-        <h1><img src='/logo.png' />Wizard News</h1>
-        <ul>
-          ${posts
-            .map((post) => {
-              return `<li>${post.title}</li>`;
-            })
-            .join('')}
-        </ul>
-      </body>
-    </html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>
   `);
 });
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(`
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+          ${post.content}
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>  
+    </div>
+  </body>
+</html>
+  `);
+});
+
+
 
 const PORT = 1337;
 
